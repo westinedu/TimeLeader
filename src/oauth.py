4013f1d72d2a56c566ed81e4a367c7f4e82cab5b
Lucas Westine <westine@gmail.com>
2012-06-18 17:12:58 +0800

    oauth and oauth2 is different

--- src/oauth.py
+++ src/oauth.py
@@ -1,19 +1,5 @@
-import oauth2 as oauth
+import oauth.oauth as oauth
 
-# Create your consumer with the proper Consumer key/Consumer secret.
-consumer_twitter = oauth.Consumer(key="IJiitWuYiHZDEEbPEJqlow", 
-    secret="hbj8OzOOS57X4PKkdEs0ShXOo57dT6Qsz1q1Rq8crFg")
-
-# Request token URL for Twitter.
-request_token_url_twitter = "http://twitter.com/oauth/request_token"
-
-# Create our client.
-client_twitter = oauth.Client(consumer_twitter)
-
-# The OAuth Client request works just like httplib2 for the most part.
-resp, content = client_twitter.request(request_token_url_twitter, "GET")
-print resp
-print content
 
 consumer_evernote = oauth.Consumer(key="westine", 
     secret="277e8b3a8cc49b2d")
